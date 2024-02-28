ebea6eb2b016070354590a04afd425a40f29d3a9
Trisinus123 <trisinusgl@gmail.com>
2024-02-28 21:21:28 +0700

    W03: Jawaban soal 4

---
+++ pbf_Praktikum 3/components/todolist.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const baseUrl = 'https://i.imgur.com/';
+const person = {
+    name: 'Gregorio Y. Zara',
+    imageId: '7vQD0fP',
+    imagesSize: 'b',
+    theme: {
+        backgroundColor: 'black',
+        color: 'pink'
+    }
+};
+
+export default function TodoList() {
+    return (
+        <div style={person.theme}>
+            <h1>{person.name}'s Todos</h1>
+            <img 
+            className="avatar"
+            src={`${baseUrl}${person.imageId}${person.imagesSize}.jpg`}
+            alt={person.name}
+            />
+            <ul>
+                <li>Improve the videophone</li>
+                <li>Prepare aeronautic lecture</li>
+                <li>Work on the alcohol-fuelled engines</li>
+            </ul>
+        </div>
+    );
+}
+
+
