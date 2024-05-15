4dfa41a81302fac0f9ae5071a1fdd7b90f27f768
Trisinus123 <trisinusgl@gmail.com>
2024-05-15 21:41:59 +0700

    Tugas Praktikum Pertemuan 9

--- pbf-pertemuan-09-nextjs-database/src/app/components/molecules/card.tsx
+++ pbf-pertemuan-09-nextjs-database/src/app/components/molecules/card.tsx
@@ -50,12 +50,13 @@ export function Card({
     return (
         <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
             <div className="flex p-4">
-                {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
-                <h3 className="ml-2 text-sm font-medium">{title}</h3>
+                {Icon ? <Icon className="h-5 w-5 text-black" /> : null} 
+                <h3 className="ml-2 text-sm font-medium" style={{ color: 'black' }}>{title}</h3> 
             </div>
             <p
                 className={`${lusitana.className}
             truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
+                style={{ color: 'black' }} 
             >
                 {value}
             </p>
